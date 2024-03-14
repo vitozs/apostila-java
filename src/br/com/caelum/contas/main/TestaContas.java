65392dbeb264904015194147e25d28f8a4fccf6d
Vitor <vitornuneschagas2016@gmail.com>
2024-03-14 15:31:56 -0300

    Exercícios 11.5: interfaces

--- src/br/com/caelum/contas/main/TestaContas.java
+++ src/br/com/caelum/contas/main/TestaContas.java
@@ -6,7 +6,7 @@ import br.com.caelum.javafx.api.main.TelaDeContas;
 public class TestaContas {
 
     public static void main(String[] args) {
-        SistemaBancario.mostraTela(false);
+        SistemaBancario.mostraTela(true);
         // TelaDeContas.main(args);
     }
 }
